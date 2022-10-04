587a6d3ceaef45862ece010c956c09cad1e2c4fd
SadiqNahmetov <sadigfn@code.edu.az>
2022-10-04 21:41:45 +0400

    Fixed: Homework

---
+++ EntityFramework/EntityFramework/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using EntityFramework.Data;
+using EntityFramework.Models;
+using EntityFramework.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Slider> sliders = _context.Sliders.ToList();
+
+
+            SliderDetail sliderDetail = _context.SliderDetails.FirstOrDefault();
+
+            HomeVM model = new HomeVM
+            {
+                Sliders = sliders,
+                SliderDetail = sliderDetail
+            };
+            return View(model);
+        }
+
+      
+
+     
+    }
+}
---
+++ EntityFramework/EntityFramework/ViewModels/HomeVM.cs
@@ -0,0 +1,15 @@
+﻿using EntityFramework.Models;
+
+using System.Collections.Generic;
+
+
+namespace EntityFramework.ViewModels
+{
+    public class HomeVM
+    {
+        public   List<Slider> MyProperty { get; set; }
+
+        public SliderDetail SliderDetail{ get; set; }
+        public List<Slider> Sliders { get; internal set; }
+    }
+}
